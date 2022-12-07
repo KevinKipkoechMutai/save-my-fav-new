b0be6c8777caea37456bcf1698559fe74551858c
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-12-07 23:53:38 +0300

    fix patch request

--- client/src/EditData.js
+++ client/src/EditData.js
@@ -42,7 +42,7 @@ export default function EditData({id, title, category, description, image_url, h
                 </label>
                 </div>
                 <div className="md:w-2/3">
-                <input  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="title" type="text" value={title} name="title" />
+                <input  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="title" type="text" value={title} name="title" defaultValue='title' />
                 </div>
             </div>
             <div className="md:flex md:items-center mb-6">
@@ -52,7 +52,7 @@ export default function EditData({id, title, category, description, image_url, h
                 </label>
                 </div>
                 <div className="md:w-2/3">
-                <input  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="category" type="text" value={category} name="category" />
+                <input  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="category" type="text" value={category} name="category" defaultValue='category'/>
                 </div>
             </div>
             <div className="md:flex md:items-center mb-6">
@@ -62,7 +62,7 @@ export default function EditData({id, title, category, description, image_url, h
                 </label>
                 </div>
                 <div className="md:w-2/3">
-                <input  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="description" type="text" value={description} name="category" />
+                <input  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="description" type="text" value={description} name="category" defaultValue='description'/>
                 </div>
             </div>
             <div className="md:flex md:items-center mb-6">
@@ -72,7 +72,7 @@ export default function EditData({id, title, category, description, image_url, h
                 </label>
                 </div>
                 <div className="md:w-2/3">
-                <input  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="image_url" type="text" value={image_url} name="image_url" />
+                <input  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="image_url" type="text" value={image_url} name="image_url" defaultValue='image_url'/>
                 </div>
             </div>
             <button className='btn btn-primary' form='editmodal' onClick={handleUpdate}>Update</button>
