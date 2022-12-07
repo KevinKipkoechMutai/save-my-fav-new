c279a4448252a2bf77a917fa0d6c789c29d3a6f8
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-12-07 18:06:17 +0300

    create and style SPA footer

---
+++ client/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer>
+            <hr/>
+            <h1>SaveMyFavs</h1>
+            <p>13 NH, Nairobi</p>
+            <p><b>Email:</b> kevin.kipkoech@student.moringaschool.com  <b>Phone:</b>+254717094454</p>
+            <p><em>Developed by: <b>Kevin Kipkoech</b></em>Ⓒ</p>
+        </footer>
+    )
+}
