5041d5c6969cafcc2bbf4302f868b2b617fbd57b
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-12-07 18:06:27 +0300

    introduction to SPA

---
+++ client/src/HeroSection.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function HeroSection() {
+    return (
+        <div className="hero-section">
+            <hr/>
+            <h1>Welcome to SaveMyFavs</h1>
+            <p>Create a collection of your favorite memories</p>
+            <p>You can even update or delete them; it's all up to you!</p>
+            <hr/>
+        </div>
+    )
+}
